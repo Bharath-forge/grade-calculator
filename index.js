4a62beb10c1c6e1ed11e23269a248799367bf0d8
Bharath-forge <bk4411336@gmail.com>
2026-04-08 00:06:11 +0530

    day 1 - variables and constants setup

---
+++ index.js
@@ -0,0 +1,7 @@
+let studentName = null;   // Will have the student's name
+let score = null;         // will have the student's score 
+const gradeA = 90;        // grade A starts at 90
+const gradeB = 80;        // grade B starts at 80
+const gradeC = 70;        // grade C starts at 70
+const gradeD = 60;        // grade D starts at 60
+const gradeF = 0;         // grade F means student has scored below 60 
