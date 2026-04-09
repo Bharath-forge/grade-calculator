97b187b6ba55ee242bbf72c7d7404a2b524bb1d6
Bharath-forge <bk4411336@gmail.com>
2026-04-09 11:42:45 +0530

    day-3 arithmetic  operations and score calculations

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
+//This is a grade calculator to evaluate their grading.
 let studentName = "Bharath";   // Assigned value to the variable
-let score = Number("75");   // Allows to convert any sting value to number
+let englishScore = 75;   // Marks secured in English subject
+let mathScore = 88;      // Marks secured in Math subject
+let physicsScore = 91;    // Marks secured in Physics subject
+let totalSubjects = 3;    // total number of subjects for which mark's received
+let total = englishScore + mathScore + physicsScore; // total of All 3 subjects
+let average = Number((total/totalSubjects).toFixed(2)); // Average of the total secured
+let finalScore = average; // final scored assigned with the average
+
 const gradeA = 90;        // grade A starts at 90
 const gradeB = 80;        // grade B starts at 80
 const gradeC = 70;        // grade C starts at 70
 const gradeD = 60;        // grade D starts at 60
 const gradeF = 0;         // grade F means student has scored below 60 
+
+console.log(total);
+console.log(average);
+console.log(finalScore);
