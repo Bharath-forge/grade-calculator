4a3b37c75a65be4178006bc70441e97c7aa3821f
Bharath-forge <bk4411336@gmail.com>
2026-04-09 11:24:16 +0530

    day 2- data types and types conversion

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-let studentName = null;   // Will have the student's name
-let score = null;         // will have the student's score 
+let studentName = "Bharath";   // Assigned value to the variable
+let score = Number("75");   // Allows to convert any sting value to number
 const gradeA = 90;        // grade A starts at 90
 const gradeB = 80;        // grade B starts at 80
 const gradeC = 70;        // grade C starts at 70
